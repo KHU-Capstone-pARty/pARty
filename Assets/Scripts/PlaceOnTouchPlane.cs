2f63f7fcd7304cf2f97148cf5278b9830eb355b0
Jangmanbo <wkddpdnjs99@gmail.com>
2022-05-13 04:25:34 +0900

    add : PlaceOnTouchPlane

---
+++ Assets/Scripts/PlaceOnTouchPlane.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+// https://www.youtube.com/watch?v=ylLP09RG_3g
+public class PlaceOnTouchPlane : MonoBehaviour
+{
+    [SerializeField]
+    private Text info;
+    [SerializeField]
+    private GameObject spawnObj;
+    [SerializeField]
+    private ARRaycastManager raycastManager;
+    private List<ARRaycastHit> hitsList = new List<ARRaycastHit>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount>0)
+        {
+            var t = Input.GetTouch(0);
+            if (t.phase==TouchPhase.Began)
+            {
+                if (raycastManager.Raycast(t.position, hitsList, TrackableType.PlaneWithinPolygon))
+                {
+                    var h = hitsList[0].pose;
+                    Instantiate(spawnObj, h.position, h.rotation);
+                    info.text = ("(" + h.position.x + ", " + h.position.y + "," + h.position.z + ")");
+                }
+            }
+        }
+    }
+}
