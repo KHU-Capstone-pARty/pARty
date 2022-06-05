3bf26b2c96d1fe220cd4d3e5da2ed4420884d62e
Jangmanbo <wkddpdnjs99@gmail.com>
2022-06-06 06:32:36 +0900

    add : GameMenu scene

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameRule;
+
+    // J : 시작하기 버튼 onclick
+    public void SelectStart()
+    {
+        SceneManager.LoadScene("MultiTestScene"); // J : scene 이동
+    }
+
+    // J : 게임방법 버튼 onclick
+    public void SelectRule()
+    {
+        Debug.Log("게임방법");
+        gameRule.SetActive(true);
+    }
+
+    // J : 게임방법 나가기 버튼 onclick
+    public void SelectRuleQuit()
+    {
+        Debug.Log("게임방법 나가기");
+        gameRule.SetActive(false);  // J : 게임방법창 비활성화
+    }
+}
